2c70a070c4e60b3547b8d64ee1c862ef0ab29ec8
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2025-12-11 19:53:22 +0530

    Setup L-9 (cors/cookieparser & utils)

---
+++ src/Utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+//Try - Catch approach
+
+/*
+steps-:
+1. const asyncHandler = () => {}
+2. const asyncHandler = (func) => () => {}
+3. const asyncHandler = (func) => async () => {}
+
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}*/
