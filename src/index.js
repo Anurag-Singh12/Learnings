2cad683c452c51de5c92d3637a79dc8e888b078d
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2025-12-11 19:25:27 +0530

    Setup L-8 db connect to atlas

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./Db/indexDb.js";
+import {app} from './app.js'
+
+
+dotenv.config({
+    path: './.env'
+})
+
+//here using seprate file to load db
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+//To conect db with first approach (all in same file)
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
