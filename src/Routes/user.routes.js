2579efd5f0804fc693041bce80eb2f0bac82ed5c
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2025-12-15 00:47:51 +0530

    L-14(multer middleware & register user )

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,11 +1,29 @@
 import { Router } from "express";
 import { registerUser } from "../Controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 //declare/initialize in variable
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(registerUser)             //http/localhost:8000/api/v1/users/register
+router.route("/register").post(
+  upload.fields([           //multer middleware
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+
+
+
+//router.route("/register").post(registerUser)             //http/localhost:8000/api/v1/users/register
 // router.route("/login").post(logincode)                //http/localhost:8000/api/v1/users/login
 
-export default router
+
+export default router;
