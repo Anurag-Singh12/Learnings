4411faadb24a6a4afcfdd3421ee16a9c3cc7690e
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2025-12-18 23:49:57 +0530

    L-15(Postman , bug fix coverimg & next function)

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
-import { User} from "../models/user.model.js"
+import { User} from "../Models/user.models.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -36,6 +36,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    //for cover img empty check
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
      if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
